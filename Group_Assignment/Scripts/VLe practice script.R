455a7bd6050a6987ee0dee8c1a094044b2d11a98
vivvle <vivianvnle@gmail.com>
2022-03-29 15:31:29 -0700

    uploading first script - V

---
+++ Group_Assignment/Scripts/VLe practice script.R
@@ -0,0 +1,20 @@
+#### Practice rstatix ####
+#### Created by: Vivian Vy Le ####
+#### Updated on: 2022-03-29 ####
+
+#### load library ####
+library(rstatix)
+library(tidyverse)
+
+#### load data ####
+
+
+#### stats analysis: test this functions ####
+#chisq_test()
+#pairwise_chisq_gof_test()
+#pairwise_chisq_test_against_p()
+
+#anova_test()
+#get_anova_test_table()
+#shaprio_test()
+#mshapiro_test()
